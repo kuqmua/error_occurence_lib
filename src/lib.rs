8f569f8d49732ac1e960261ce6372547ef87fe10
kuqmua <serezhashmakov@outlook.com>
2024-01-26 02:16:41 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ pub mod error_occurence_unnamed;
 // pub mod vec_to_string_without_config_to_string;
 
 // pub mod get_code_path_without_config;
-// pub mod get_column;
+pub mod get_column;
 // pub mod get_duration;
 pub mod get_file;
 pub mod get_line;
