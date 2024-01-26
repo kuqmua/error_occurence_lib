ebbfe6b55bfee991ff4f2d65ab63456ad7c177e5
kuqmua <serezhashmakov@outlook.com>
2024-01-26 05:09:40 +0300

    next steps

--- src/lines_space_backslash.rs
+++ src/lines_space_backslash.rs
@@ -1,12 +1,12 @@
 pub trait LinesSpaceBackslash {
-    fn lines_space_backslash(&self) -> String;
+    fn lines_space_backslash(&self) -> std::string::String;
 }
 
 impl<SelfGeneric> LinesSpaceBackslash for SelfGeneric
 where
     SelfGeneric: std::fmt::Display,
 {
-    fn lines_space_backslash(&self) -> String {
+    fn lines_space_backslash(&self) -> std::string::String {
         self.to_string()
             .lines()
             .fold(String::from(""), |mut acc, line| {
